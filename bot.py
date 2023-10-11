8e1289e44b906fe29eeadc56158932ae2da968c1
Oleh Kinash <o.qinash@gmail.com>
2023-10-11 19:54:06 +0300

    first commit

---
+++ bot.py
@@ -0,0 +1,25 @@
+import os
+import settings as settings
+import telebot
+
+bot = telebot.TeleBot(token = settings.BOT_TOKEN)
+
+
+@bot.message_handler(commands=['start', 'hello'])
+def send_welcome(message):
+    bot.send_message(settings.USER_TOKEN, "Хельо, " + message.from_user.first_name)
+
+
+@bot.message_handler(func=lambda msg: True)
+def echo_all(message):
+    bot.send_message(message.from_user.id, message.from_user.first_name + ", не базарь, а дай цьом Миколаїчу")
+
+
+@bot.message_handler(content_types=['photo'])
+def photo(message):
+    bot.send_message(message.from_user.id, "Це Кора")
+
+
+
+bot.infinity_polling()
+
---
+++ main.py
@@ -0,0 +1,38 @@
+from typing import Union
+
+import uvicorn
+from fastapi import FastAPI
+
+from model.models import *
+
+
+app = FastAPI()
+# data = get_data('insurance')
+# s = setup(data, target='charges')
+# best = compare_models()
+# create_api(best, 'insurance_prediction_model')
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.put("/updateItem/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
+
+@app.post('/predict')
+def predict(age, sex, bmi, children, smoker, region):
+    return 'Hello'
+
+
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8000)
